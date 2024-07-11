6d12b26170ea4b299f37e75a9346a9ec5467fe88
Kunihiko Miyanaga <miyanaga@ideamans.com>
2024-07-11 12:35:53 +0900

    サイトスピードの重要指標に関する記事を作成

--- .vitepress/theme/Author.vue
+++ .vitepress/theme/Author.vue
@@ -8,7 +8,8 @@ const { frontmatter } = useData()
   <dl class="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 dark:xl:border-slate-200/5">
     <dt class="sr-only">Authors</dt>
     <dd>
-      <ul class="flex justify-center xl:block space-x-8 sm:space-x-12 xl:space-x-0 xl:space-y-8">
+      <ul
+        class="flex items-start md:items-center justify-center xl:block space-x-8 sm:space-x-12 xl:space-x-0 xl:space-y-8 gap-8">
         <li class="flex items-center space-x-2">
           <img v-if="frontmatter.gravatar" :src="'https://gravatar.com/avatar/' + frontmatter.gravatar"
             alt="author image" class="w-10 h-10 rounded-full" />
