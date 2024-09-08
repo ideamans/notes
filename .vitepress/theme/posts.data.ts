363a1b3e88d9beb8c9859a3874e5f9c9a98b5ba6
Kunihiko Miyanaga <miyanaga@ideamans.com>
2024-09-08 21:16:48 +0900

    postsに年フォルダを追加。記事を修正

--- .vitepress/theme/posts.data.ts
+++ .vitepress/theme/posts.data.ts
@@ -13,7 +13,7 @@ interface Post {
 declare const data: Post[]
 export { data }
 
-export default createContentLoader('posts/*.md', {
+export default createContentLoader('posts/**/*.md', {
   excerpt: true,
   transform(raw): Post[] {
     return raw
