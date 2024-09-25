3a67f871d825eb6a603080e9c1819bae41565371
Kunihiko Miyanaga <miyanaga@ideamans.com>
2024-09-26 08:22:20 +0900

    X用の画像を追加

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,38 @@ import Dayjs from 'dayjs'
 import { withMermaid } from 'vitepress-plugin-mermaid'
 import { genNotesFeed } from './genFeed.js'
 
+function indexImageUrl(bgUrl: string, subTitle: string): string {
+  const ogp = new URL('https://banners.ideamans.com/banners/type-a')
+  ogp.searchParams.set('bgUrl', bgUrl)
+
+  ogp.searchParams.set('text0', `ideaman's Notes`)
+  ogp.searchParams.set('text0width', '60%')
+
+  ogp.searchParams.set('text1', subTitle)
+  ogp.searchParams.set('text1width', '60%')
+
+  return ogp.href
+}
+
+function articleImageUrl(bgUrl: string, title: string, meta: string): string {
+  const ogp = new URL('https://banners.ideamans.com/banners/type-a')
+  ogp.searchParams.set('bgUrl', bgUrl)
+
+  ogp.searchParams.set('text0', `ideaman's Notes`)
+  ogp.searchParams.set('text0width', '60%')
+
+  ogp.searchParams.set('text1', title)
+  ogp.searchParams.set('texts[1].fontSize', '5%')
+  ogp.searchParams.set('texts[1].minWidth', '60%')
+  ogp.searchParams.set('texts[1].maxWidth', '90%')
+  ogp.searchParams.set('text2', meta)
+  ogp.searchParams.set(`text[2].fontSize`, '3%')
+  ogp.searchParams.set(`text[2].minWidth`, '30%')
+  ogp.searchParams.set(`text[2].maxWidth`, '40%')
+
+  return ogp.href
+}
+
 export default withMermaid({
   title: `ideaman's Notes`,
   description: 'アイデアマンズ株式会社の研究ノート',
@@ -55,35 +87,47 @@ gtag('config', 'G-YQBLSY0PKS');
   ],
   buildEnd: genNotesFeed,
   transformHead: ({ head, pageData }) => {
-    const ogp = new URL('https://banners.ideamans.com/banners/type-a')
-    ogp.searchParams.set(
-      'bgUrl',
-      'https://notes.ideamans.com/ogp-background.jpg'
-    )
-    ogp.searchParams.set('text0', `ideaman's Notes`)
-    ogp.searchParams.set('text0width', '60%')
+    const ogpBgUrl = 'https://notes.ideamans.com/ogp-background.jpg'
+    const xBgUrl = 'https://notes.ideamans.com/x-background.jpg'
 
-    if (pageData.frontmatter.index) {
-      // トップページ
-      ogp.searchParams.set('text1', pageData.frontmatter.subtext)
-      ogp.searchParams.set('text1width', '60%')
+    if (pageData.frontmatter?.index) {
+      // インデックスページ
+      const subTitle = pageData.frontmatter.subtext
+      head.push([
+        'meta',
+        {
+          property: 'og:image',
+          content: indexImageUrl(ogpBgUrl, pageData.frontmatter.subtext)
+        }
+      ])
+      head.push([
+        'meta',
+        {
+          property: 'twitter:image',
+          content: indexImageUrl(xBgUrl, pageData.frontmatter.subtext)
+        }
+      ])
     } else {
       // 記事ページ
       const title = pageData.frontmatter.title
       const id = pageData.frontmatter.id
       const date = Dayjs(pageData.frontmatter.date).format('YYYY-MM-DD')
 
-      ogp.searchParams.set('text1', title)
-      ogp.searchParams.set('texts[1].fontSize', '5%')
-      ogp.searchParams.set('texts[1].minWidth', '60%')
-      ogp.searchParams.set('texts[1].maxWidth', '90%')
-      ogp.searchParams.set('text2', `${date} @${id}`)
-      ogp.searchParams.set(`text[2].fontSize`, '3%')
-      ogp.searchParams.set(`text[2].minWidth`, '30%')
-      ogp.searchParams.set(`text[2].maxWidth`, '40%')
+      head.push([
+        'meta',
+        {
+          property: 'og:image',
+          content: articleImageUrl(ogpBgUrl, title, `${date} @${id}`)
+        }
+      ])
+      head.push([
+        'meta',
+        {
+          property: 'twitter:image',
+          content: articleImageUrl(xBgUrl, title, `${date} @${id}`)
+        }
+      ])
     }
-
-    head.push(['meta', { property: 'og:image', content: ogp.href }])
   },
   appearance: false
 })
