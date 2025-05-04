411f6baaaae2a528f165be5006d6aac2832a6bcf
Kunihiko Miyanaga <miyanaga@ideamans.com>
2025-05-04 15:25:03 +0900

    RSSフィードのURLを修正

--- .vitepress/genFeed.ts
+++ .vitepress/genFeed.ts
@@ -3,54 +3,6 @@ import { writeFileSync } from 'fs'
 import { Feed } from 'feed'
 import { createContentLoader, type SiteConfig } from 'vitepress'
 
-const baseUrl = `https://blog.vuejs.org`
-
-export async function genFeed(config: SiteConfig) {
-  const feed = new Feed({
-    title: 'The Vue Point',
-    description: 'The official blog for the Vue.js project',
-    id: baseUrl,
-    link: baseUrl,
-    language: 'en',
-    image: 'https://vuejs.org/images/logo.png',
-    favicon: `${baseUrl}/notes.svg`,
-    copyright:
-      'Copyright (c) 2021-present, Yuxi (Evan) You and blog contributors'
-  })
-
-  const posts = await createContentLoader('posts/*.md', {
-    excerpt: true,
-    render: true
-  }).load()
-
-  posts.sort(
-    (a, b) =>
-      +new Date(b.frontmatter.date as string) -
-      +new Date(a.frontmatter.date as string)
-  )
-
-  for (const { url, excerpt, frontmatter, html } of posts) {
-    feed.addItem({
-      title: frontmatter.title,
-      id: `${baseUrl}${url}`,
-      link: `${baseUrl}${url}`,
-      description: excerpt,
-      content: html?.replaceAll('&ZeroWidthSpace;', ''),
-      author: [
-        {
-          name: frontmatter.author,
-          link: frontmatter.twitter
-            ? `https://twitter.com/${frontmatter.twitter}`
-            : undefined
-        }
-      ],
-      date: frontmatter.date
-    })
-  }
-
-  writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2())
-}
-
 const notesUrl = 'https://notes.ideamans.com'
 
 export async function genNotesFeed(config: SiteConfig) {
@@ -61,7 +13,7 @@ export async function genNotesFeed(config: SiteConfig) {
     link: notesUrl,
     language: 'ja',
     image: 'https://alogorithm2.ideamans.com/v2/rect.svg?width=800&seed=notes',
-    favicon: `${baseUrl}/notes.svg`,
+    favicon: `${notesUrl}/notes.svg`,
     copyright: `Copyright (c) 2024-present, ideaman's Inc.`
   })
 
@@ -79,8 +31,8 @@ export async function genNotesFeed(config: SiteConfig) {
   for (const { url, excerpt, frontmatter, html } of posts) {
     feed.addItem({
       title: frontmatter.title,
-      id: `${baseUrl}${url}`,
-      link: `${baseUrl}${url}`,
+      id: `${notesUrl}${url}`,
+      link: `${notesUrl}${url}`,
       description: excerpt,
       content: excerpt, // html?.replaceAll('&ZeroWidthSpace;', ''),
       author: [
